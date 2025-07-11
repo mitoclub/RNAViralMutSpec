5019cd3be6c7f91c3d5536a04345263842523c85
kpotoh <axepon@mail.ru>
2025-07-11 18:47:24 +0200

    site categorizaiton gamma done

--- sars-cov-2_aa_subst/verify_model_site_rates.py
+++ sars-cov-2_aa_subst/verify_model_site_rates.py
@@ -74,6 +74,9 @@ def main():
     #     .assign(count=1)['count'].rename(index=amino_acid_codes).to_dict()
     # print(uniform_aa_content)
 
+    # read site rates
+    site_rates_df = pd.read_csv('data/ref_sites_rates_cat4.csv')
+
     metrics_total = []
     for clade in tqdm.tqdm(clades_spectra.clade.unique(), desc='Clades'):
         spectrum_clade = clades_spectra[clades_spectra['clade'] == clade]
@@ -84,11 +87,12 @@ def main():
         obs_clade = obs_clade[(obs_clade.aa1 != '*') & (obs_clade.aa2 != '*')]
 
         # site_rates = obs_raw.query('noncoding == False').groupby('nt_site')['count'].sum()
-        site_rates = obs_clade.groupby('nt_site')['count'].sum()
-        genome_intervals = np.linspace(0, len(site_rates), 11).astype(int)
+        # site_rates = obs_clade.groupby('nt_site')['count'].sum()
+        # genome_intervals = np.linspace(0, len(site_rates), 11).astype(int)
+        # for i, (x1, x2) in enumerate(zip(genome_intervals[:-1], genome_intervals[1:])):
 
-        for i, (x1, x2) in enumerate(zip(genome_intervals[:-1], genome_intervals[1:])):
-            cur_sites = site_rates.sort_values().iloc[x1: x2].index.values
+        for i, rcat in enumerate(range(1, site_rates_df.rate_cat.max()+1)):
+            cur_sites = site_rates_df.query('rate_cat == @rcat').site.tolist()
             df_obs = obs_clade[obs_clade.nt_site.isin(cur_sites)]
             
             cur_aa_freqs_dct = get_site_specific_aa_counts(cur_sites)
@@ -97,7 +101,7 @@ def main():
             cur_metrics = calc_metrics(aa_subst)
             cur_metrics['nt_sites'] = df_obs.nt_site.nunique()
             cur_metrics['clade'] = clade
-            cur_metrics['sites_sample'] = i+1
+            cur_metrics['rate_cat'] = rcat
             metrics_total.append(cur_metrics)
 
         # for total sites set
@@ -105,45 +109,16 @@ def main():
         cur_metrics = calc_metrics(aa_subst)
         cur_metrics['nt_sites'] = obs_clade.nt_site.nunique()
         cur_metrics['clade'] = clade
-        cur_metrics['sites_sample'] = 'total'
+        cur_metrics['rate_cat'] = 'any'
         metrics_total.append(cur_metrics)
 
-    metrics_total_df = pd.DataFrame(metrics_total).set_index(['clade', 'sites_sample', 'nt_sites'])\
+    metrics_total_df = pd.DataFrame(metrics_total).set_index(['clade', 'rate_cat', 'nt_sites'])\
             .drop(['mape','wape','slope','intercept','pearson_corr','pearson_p',
                    'ks_stat','ks_p','rmse','log_likelihood'], axis=1)
-    metrics_total_df.to_csv('data/fit_metrics_site_rates10.csv', float_format='%g')
+    metrics_total_df.to_csv('data/fit_metrics_site_rate_cat4.csv', float_format='%g')
 
 
-    # _ = metrics_total_df[['spearman_corr','r2', 'wape', 'mut_count']]\
-    #     .melt(ignore_index=False, var_name='metric').reset_index()
-    # g = sns.catplot(data=_, sharey=False, kind='box', col='metric', col_wrap=2,
-    #                 y='value', x='sites_sample',
-    #                 palette='Set2', height=3, aspect=1.25,
-    # )
-    # g.set_titles('{col_name}')
-    # g.set_xticklabels(g.axes[2].get_xticklabels(), rotation=-45)
-    # g.set_xlabels('Sites')
-    # g.set_ylabels('Metric value')
-    # g.axes_dict['mut_count'].set_yscale('log')
-    # g.legend.set_title('Site sampling\ncutoff, %')
-    # g.savefig('./figures/fit_metrics_site_rates.pdf')
-
-    # _ = metrics_total_df[['r2']]\
-    #     .melt(ignore_index=False, var_name='metric').reset_index()
-    # g = sns.catplot(data=_, sharey=False, kind='box', col='metric', col_wrap=1,
-    #                 y='value', x='sites_sample',
-    #                 palette='Set2', height=4, aspect=1.2,
-    #                 # col_order=['accuracy']
-    # )
-    # g.set_titles('')
-    # g.set_xticklabels(g.axes[0].get_xticklabels(), rotation=-25)
-    # g.set_xlabels('Sites')
-    # g.set_ylabels('$R^2$')
-    # g.legend.set_title('Site sampling\ncutoff, %')
-    # plt.grid(axis='y')
-    # g.savefig('./figures/fit_metrics_site_rates_r2.pdf')
-
-    print(metrics_total_df.reset_index().query('sites_sample == "total"')\
+    print(metrics_total_df.reset_index().query('rate_cat == "any"')\
         .set_index('clade')[['r2', 'spearman_corr', 'mut_count']].round(2))
 
 
